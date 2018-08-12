cce36e5638b4ca69e359acd0acedf5413a0a1290
PawelKowalewski1 <p.m.kowalewski@gmail.com>
2018-08-12 16:32:48 +0200

    w3mikniblog 1.commit

---
+++ src/main/java/pl/reaktor/w3/miniblog/W3MiniblogApplication.java
@@ -0,0 +1,12 @@
+package pl.reaktor.w3.miniblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3MiniblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3MiniblogApplication.class, args);
+	}
+}
